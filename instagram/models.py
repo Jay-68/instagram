5e4591845caa122b27f71fb3c780e70cb663e715
Jay-68 <kelvinngari1995@gmail.com>
2019-11-17 10:54:40 +0300

    add a comments and profile model for the users

--- instagram/models.py
+++ instagram/models.py
@@ -6,6 +6,42 @@ from django.db import models
 # Create your models here.
 
 
+class Profile(models.Model):
+    """
+    Class that contains profile details
+    """
+    bio = HTMLField()
+    dp = models.ImageField(upload_to='images/', blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null="True")
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+    post_save.connect(save_user_profile, sender=User)
+
+    def save_profile(self):
+        self.save()
+
+    def del_profile(self):
+        self.delete()
+
+    @classmethod
+    def search_profile(cls, name):
+        profile = cls.objects.filter(user__username__icontains=name)
+        return profile
+
+    @classmethod
+    def get_by_id(cls, id):
+        profile = Profile.objects.get(id=id)
+        return profile
+
+
 class Image(models.Model):
     post = models.ImageField(upload_to='images/', blank=True)
     caption = HTMLField()
@@ -29,3 +65,41 @@ class Image(models.Model):
     def get_images(cls):
         images = Image.objects.all()
         return images
+
+    @property
+    def count_likes(self):
+        likes = self.likes.count()
+        return likes
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        image = Image.objects.filter(user_id=id).all()
+        return image
+
+
+class Comments(models.Model):
+    """
+    Class that contains comments details
+    """
+    comment = HTMLField()
+    posted_on = models.DateTimeField(auto_now=True)
+    image = models.ForeignKey(
+        Image, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null="True")
+
+    def __str__(self):
+        return self.comment
+
+    class Meta:
+        ordering = ['posted_on']
+
+    def save_comm(self):
+        self.save()
+
+    def del_comm(self):
+        self.delete()
+
+    @classmethod
+    def get_comments_by_image_id(cls, image):
+        comments = Comments.objects.get(image_id=image)
+        return comments
