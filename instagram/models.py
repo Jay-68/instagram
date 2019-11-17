5766798e016d9c6bce672ea92dcfecdf2db0e9b1
Jay-68 <kelvinngari1995@gmail.com>
2019-11-17 10:27:45 +0300

    Add an image model for user posts

--- instagram/models.py
+++ instagram/models.py
@@ -4,3 +4,23 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Image(models.Model):
+    post = models.ImageField(upload_to='images/', blank=True)
+    caption = HTMLField()
+    posted_on = models.DateTimeField(auto_now_add=True)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null='True')
+
+    def __str__(self):
+        return self.caption
+
+    class Meta:
+        ordering = ['posted_on']
+
+    def save_img(self):
+        self.save()
+
+    def delete_img(self):
+        self.delete()
