009e1431894dde7674ee1a29f36b11d029f01556
Jay-68 <kelvinngari1995@gmail.com>
2019-11-17 12:02:06 +0300

    Add routing for user profile and bio in the application

--- instagram/views.py
+++ instagram/views.py
@@ -1,16 +1,24 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.shortcuts import render
-from .models import Image, Profile, Comments, Likes
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from .forms import SignupForm
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
 from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
+from .forms import ImageForm, ProfileForm, CommentsForm
+from .models import Image, Profile, Comments, Likes
 from friendship.models import Friend, Follow, Block
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def index(request):
     '''
-    Function that returns website application landing page.
+    Function that returns website application home page.
     '''
     images = Image.get_images().order_by('-posted_on')
     profiles = User.objects.all()
@@ -36,10 +44,33 @@ def new_post(request):
                 image.profile = profile
                 image.user = request.user
                 image.save()
-            return redirect('landing')
+            return redirect('home')
         else:
             form = ImageForm()
     return render(request, 'new_post.html', {"form": form})
+
+
+@login_required(login_url='/accounts/login/')
+def like_post(request):
+    image = get_object_or_404(Image, id=request.POST.get('image_id'))
+    image.likes.add(request.user)
+    return redirect('home')
+
+
+@login_required(login_url='/accounts/login/')
+def profile(request, user_id):
+    """
+    Function that enables one to see their profile
+    """
+    title = "Profile"
+    images = Image.get_image_by_id(id=user_id).order_by('-posted_on')
+    comments = Comments.objects.all()
+    profiles = User.objects.get(id=user_id)
+    users = User.objects.get(id=user_id)
+    follow = len(Follow.objects.followers(users))
+    following = len(Follow.objects.following(users))
+    people = Follow.objects.following(request.user)
+    return render(request, 'profile/profile.html', {'title': title, "images": images, "follow": follow, "following": following, "profiles": profiles, "people": people, "comments": comments})
 
 
 @login_required(login_url='/accounts/login/')
@@ -54,7 +85,7 @@ def edit_profile(request):
         if form.is_valid():
             profile = form.save(commit=False)
             profile.save()
-        return redirect('landing')
+        return redirect('home')
     else:
         form = ProfileForm()
     return render(request, 'profile/edit-profile.html', {"form": form, })
@@ -65,7 +96,7 @@ def follow(request, user_id):
     other_user = User.objects.get(id=user_id)
     follow = Follow.objects.add_follower(request.user, other_user)
 
-    return redirect('landing')
+    return redirect('home')
 
 
 @login_required(login_url='/accounts/login/')
@@ -74,7 +105,7 @@ def unfollow(request, user_id):
 
     follow = Follow.objects.remove_follower(request.user, other_user)
 
-    return redirect('landing')
+    return redirect('home')
 
 
 @login_required(login_url='/accounts/login/')
@@ -106,11 +137,14 @@ def add_comment(request, image_id):
             comment.user = request.user
             comment.image = images
             comment.save()
-    return redirect('landing')
+    return redirect('home')
 
 
-@login_required(login_url='/accounts/login/')
-def like_post(request):
-    image = get_object_or_404(Image, id=request.POST.get('image_id'))
-    image.likes.add(request.user)
-    return redirect('landing')
+def like(request, image_id):
+    current_user = request.user
+    liked_post = Image.objects.get(id=image_id)
+    new_like, created = Likes.objects.get_or_create(
+        user_like=current_user, liked_post=liked_post)
+    new_like.save()
+
+    return redirect('home')
